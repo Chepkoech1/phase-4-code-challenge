ec6d460fb5eeaa54d269ef835d6000e6d7a5cad8
Chepkoech1 <sandrachepkoech60@gmail.com>
2023-03-21 11:49:55 +0300

    changed files

---
+++ pizzas/app/controllers/pizzas_controller.rb
@@ -0,0 +1,8 @@
+class PizzasController < ApplicationController
+    def index
+        @pizzas = Pizza.all
+        render json: @pizzas.to_json(only: [:id, :name, :ingredients])
+    end
+      
+      
+end
